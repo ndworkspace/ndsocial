e63d094af41523a6a17047b9bc437c0d0e4b0379
totaltang <christangtt@gmail.com>
2014-05-08 19:29:42 +0800

    解析返回联系人好友关系

--- src/cn/nd/social/net/AccountInfoParser.java
+++ src/cn/nd/social/net/AccountInfoParser.java
@@ -9,13 +9,14 @@ import org.json.JSONObject;
 public class AccountInfoParser {
 	public static void parseContactFriendInfo(String info, List<String>friendMobile,List<String>notFriendMobiles,List<Long>notFriendIds) {
 		try {
-			JSONArray jArray = new JSONArray(info);
+			JSONObject jinfo = new JSONObject(info);
+			JSONArray jArray =  jinfo.getJSONArray("PHONELIST");
 			for(int i=0;i <jArray.length();i++) {
 				JSONObject jobj = (JSONObject)jArray.get(i);
 				String phone = jobj.getString("PHONE");
 				String isFriend = jobj.getString("FRIEND");
 				String uid = jobj.getString("UID");
-				if("yes".equals(isFriend)) {
+				if("YES".equals(isFriend)) {
 					friendMobile.add(phone);
 				} else {
 					notFriendMobiles.add(phone);
